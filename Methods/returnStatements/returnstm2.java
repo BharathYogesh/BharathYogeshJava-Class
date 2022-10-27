dec61f8606a1db5b512733c1fa06b00e8ead8fbf
BharathYogesh <dnbharath36@gmail.com>
2022-10-27 09:31:42 +0530

    Return

---
+++ Methods/returnStatements/returnstm2.java
@@ -0,0 +1,13 @@
+public class returnstm2
+{
+static int myMethod(int y){
+return y-3;
+}
+
+public static void main (String args[]){
+System.out.println(myMethod(9));
+System.out.println(myMethod(8/2));
+System.out.println(myMethod(4/2));
+
+}
+}
---
+++ Methods/returnStatements/returnstm3.java
@@ -0,0 +1,12 @@
+public class returnstm3
+{
+static int myMethod(int x, int y){
+return  x+y;
+}
+
+public static void main (String args[]){
+int z= myMethod(9,5);
+System.out.println(z);
+
+}
+}
