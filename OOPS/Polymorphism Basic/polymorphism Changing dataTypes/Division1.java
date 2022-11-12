4bbdbf52ad0d714200b7571be5bef609c905e893
BharathYogesh <dnbharath36@gmail.com>
2022-11-12 20:32:36 +0530

    Polymorphism Changing Its datatypes

---
+++ OOPS/Polymorphism Basic/polymorphism Changing dataTypes/Division1.java
@@ -0,0 +1,25 @@
+package com.Oops_Polymorphism_Method_Overloading.Changing_datatypes;
+
+public class Division1
+{
+
+public static int div(int b, int c)
+{
+	int div = (b/c);
+	return div;
+			
+}
+
+public static  double div1(double s,double p)
+{	
+	double div1=(s/p);
+	return div1;
+}
+
+public static void main (String args[])
+{
+System.out.println(div(100,2));
+System.out.println(div1(100.2,50.1));
+
+}
+}
