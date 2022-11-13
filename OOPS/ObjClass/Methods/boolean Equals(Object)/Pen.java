c63cce08ebc89b5e0144e9f884dc473b1b572d1e
BharathYogesh <dnbharath36@gmail.com>
2022-11-13 13:40:02 +0530

    Boolean methods

---
+++ OOPS/ObjClass/Methods/boolean Equals(Object)/Pen.java
@@ -0,0 +1,21 @@
+package ObjClass;
+
+public class Pen {
+String name;
+String price;
+
+
+
+public Pen(String name,String price)
+{
+	this.name=name;
+	this.price=price;
+}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		return super.equals(obj);
+	}
+	
+}
---
+++ OOPS/ObjClass/Methods/boolean Equals(Object)/TestOfBooleanEqulas.java
@@ -0,0 +1,16 @@
+package ObjClass;
+
+public class TestOfBooleanEqulas{
+
+	
+	public static void main (String args[])
+	{
+		Pen cello =new Pen("Cello","205rs");
+		Pen breite=new Pen("Brite","52rs");
+		Pen cello1=new Pen("Cello","205rs");
+		System.out.println(cello.equals(breite));
+		System.out.println(cello.equals(cello));
+				
+				
+	}
+}
