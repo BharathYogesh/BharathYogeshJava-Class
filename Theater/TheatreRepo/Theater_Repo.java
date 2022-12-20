f594e8142f24b622da47ba85c6279af82c930ae6
BharathYogesh <dnbharath36@gmail.com>
2022-12-20 23:47:07 +0530

    Repository

---
+++ Theater/TheatreRepo/Theater_Repo.java
@@ -0,0 +1,8 @@
+package com.xworkz_theater_repo;
+
+import com.Xworkz_Theater.TheaterDto;
+
+public interface Theater_Repo {
+ public boolean save(TheaterDto theaterdto);
+ public boolean read(TheaterDto theaterdto);
+}
