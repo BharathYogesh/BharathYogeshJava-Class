5c51be115c7a8d2167342e49fbfd1d49160bd67e
BharathYogesh <dnbharath36@gmail.com>
2022-12-21 08:50:26 +0530

    programs

---
+++ methodparametersargs1.java
@@ -0,0 +1,10 @@
+public class methodparametersargs1
+{
+static void myMethod(String name,int age){
+System.out.println(name+ " is " +age);
+}
+public static void main(String args[]){
+myMethod("bharath",  25);
+
+}
+}
---
+++ returnstm1.java
@@ -0,0 +1,13 @@
+public class returnstm1
+{
+static int myMethod(int x){
+return 5+x;
+}
+
+public static void main(String args[]){
+System.out.println(myMethod(9));
+System.out.println(myMethod(6));
+System.out.println(myMethod(8));
+System.out.println(myMethod(2));
+}
+}
